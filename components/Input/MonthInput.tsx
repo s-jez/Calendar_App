b828e7c7914abb20cd41f43c8a37125e920bb0f3
s-jez <stanislawjez7@gmail.com>
2022-11-19 22:02:34 +0100

    add functions to handle next months.

--- components/Input/MonthInput.tsx
+++ components/Input/MonthInput.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../Input/Input.module.scss";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import { CALENDAR_MONTH_NAMES } from "../../helpers/calendar";
+import { getNextMonth, getPrevMonth } from "../../helpers/getMonthDate";
 
 interface IMonthInput {
   month: number;
@@ -9,15 +10,23 @@ interface IMonthInput {
 }
 
 const MonthInput = ({ month, year }: IMonthInput) => {
+  const handlePrevMonth = () => {
+    const date = Object.assign(getPrevMonth(month, year));
+    console.log(date);
+  };
+  const handleNextMonth = () => {
+    const date = Object.assign(getNextMonth(month, year));
+    console.log(date);
+  };
   return (
     <div className={styles.month}>
-      <div className={styles.arrow}>
+      <div className={styles.arrow} onClick={handlePrevMonth}>
         <SlArrowLeft />
       </div>
       <div className={styles.bold}>
         {CALENDAR_MONTH_NAMES[month - 1]} {year}
       </div>
-      <div className={styles.arrow}>
+      <div className={styles.arrow} onClick={handleNextMonth}>
         <SlArrowRight />
       </div>
     </div>
